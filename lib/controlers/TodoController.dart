838c36708651f9bd46887da526ebe0af5a02e733
mlatifr <mlatifraldi@gmail.com>
2022-06-06 19:50:00 +0700

    get Storage on TodoController

--- lib/controlers/TodoController.dart
+++ lib/controlers/TodoController.dart
@@ -1,7 +1,19 @@
 import 'package:flutter_getx_todo_list/models/Todo.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class TodoController extends GetxController {
   var todos = List<Todo>().obs;
-  
+
+  @override
+  void onInit() {
+    List storedTodos = GetStorage().read<List>('todos');
+    if (storedTodos != null) {
+      todos = storedTodos.map((e) => Todo.fromJson(e)).toList().obs;
+    }
+    ever(todos, (_) {
+      GetStorage().write('todos', todos.toList());
+    });
+    super.onInit();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getx_todo_list/screens/HomeScreen.dart';
 import 'package:flutter_getx_todo_list/screens/TodoScreen.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() {
+  GetStorage.init();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeScreen(),
